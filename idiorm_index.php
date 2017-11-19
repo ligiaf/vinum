82381f0e252525e3288d980071e2cc0b8ef5d736
raffael-carvalho <ffaelcdec@gmail.com>
2017-11-19 19:11:01 -0200

    idiorm

--- idiorm_index.php
+++ idiorm_index.php
@@ -3,6 +3,8 @@
 require_once 'vendor/autoload.php';
 include 'conecta.php';
 
+$testeClasse = new classes\Teste();
+
 $vinhos = ORM::for_table('regiao')->find_many();
 
 foreach ($vinhos as $vinho) {
